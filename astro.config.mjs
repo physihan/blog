7b5f6772f764d8623b349b43b4cd5dfc1f91e38d
physihan <physihan@outlook.com>
2023-04-28 22:27:40 +0800

    feat: 增加响应式布局

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,7 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  output: 'server',
   integrations: [mdx(), sitemap(), tailwind(), solidJs()],
-  adapter: vercel({
-    analytics: true
-  })
+
 });
 
